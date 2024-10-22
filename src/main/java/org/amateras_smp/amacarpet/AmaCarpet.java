7eb347fd6d67cf3439e3dd5353bac87224867c38
pugur <ryota52399@gmail.com>
2024-10-23 06:24:51 +0900

    update dependency

--- src/main/java/org/amateras_smp/amacarpet/AmaCarpet.java
+++ src/main/java/org/amateras_smp/amacarpet/AmaCarpet.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AmaCarpet implements ModInitializer {
+public class AmaCarpet implements CarpetExtension, ModInitializer {
 
     public static String MOD_NAME = "AmaCarpet";
     public static String MOD_ID = "ama-carpet";
