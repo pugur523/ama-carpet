d0dfa02537661908ba57e30d49b470a541b52341
pugur <ryota52399@gmail.com>
2024-10-24 16:18:50 +0900

    small update

--- src/main/java/org/amateras_smp/amacarpet/mixins/chunk_tickets/MixinServerWorld.java
+++ src/main/java/org/amateras_smp/amacarpet/mixins/chunk_tickets/MixinServerWorld.java
@@ -7,9 +7,9 @@ import net.minecraft.server.world.ChunkTicketType;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
-import org.amateras_smp.amacarpet.AmaCarpet;
 import org.amateras_smp.amacarpet.AmaCarpetSettings;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,31 +19,24 @@ public abstract class MixinServerWorld {
 
     //#if MC < 12100
     // this was implemented in 1.21 so is not needed in 1.21 or above
-    @Inject(method = "onPlayerChangeDimension",
-            at = @At("TAIL")
-    )
-    private void onPlayerDimensionChanged(ServerPlayerEntity player, CallbackInfo ci) {
+    @Unique
+    private void issuePortalTicket(Entity entity) {
         if (!AmaCarpetSettings.endPortalChunkLoad) return;
         ServerWorld world = (ServerWorld) (Object) this;
         if (world.getRegistryKey() == ServerWorld.END) {
-            BlockPos pos = player.getBlockPos();
-            AmaCarpet.LOGGER.info("debugging : onPlayerDimensionChanged");
+            BlockPos pos = entity.getBlockPos();
             world.getChunkManager().addTicket(ChunkTicketType.PORTAL, new ChunkPos(pos), 3, pos);
         }
     }
 
-    @Inject(method = "onDimensionChanged",
-            at = @At("TAIL")
-    )
+    @Inject(method = "onPlayerChangeDimension", at = @At("TAIL"))
+    private void onPlayerDimensionChanged(ServerPlayerEntity player, CallbackInfo ci) {
+        issuePortalTicket(player);
+    }
+
+    @Inject(method = "onDimensionChanged", at = @At("TAIL"))
     private void onEntityDimensionChanged(Entity entity, CallbackInfo ci) {
-        if (!AmaCarpetSettings.endPortalChunkLoad) return;
-        AmaCarpet.LOGGER.info("debugging : onEntityDimensionChanged");
-        ServerWorld world = (ServerWorld) (Object) this;
-        if (world.getRegistryKey() == ServerWorld.END) {
-            AmaCarpet.LOGGER.info("debugging : world is end");
-            BlockPos blockPos = entity.getBlockPos();
-            world.getChunkManager().addTicket(ChunkTicketType.PORTAL, new ChunkPos(blockPos), 3, blockPos);
-        }
+        issuePortalTicket(entity);
     }
     //#endif
 }
