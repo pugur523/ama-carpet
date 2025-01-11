e2b93cd4e7d1a843c2dab844eade11549b9ea868
pugur <ryota52399@gmail.com>
2025-01-12 05:11:16 +0900

    ?

--- src/main/java/org/amateras_smp/amacarpet/client/utils/ClientModUtil.java
+++ src/main/java/org/amateras_smp/amacarpet/client/utils/ClientModUtil.java
@@ -1,6 +1,8 @@
 package org.amateras_smp.amacarpet.client.utils;
 
+import com.google.common.collect.ImmutableList;
 import net.fabricmc.loader.api.FabricLoader;
+import org.amateras_smp.amacarpet.AmaCarpet;
 import org.amateras_smp.amacarpet.client.AmaCarpetClient;
 
 import java.lang.reflect.Field;
@@ -8,8 +10,42 @@ import java.lang.reflect.Modifier;
 import java.util.HashMap;
 
 public class ClientModUtil {
-    public static boolean isFeatureEnable(String modId, String className, String fieldName) {
-        if (!FabricLoader.getInstance().isModLoaded(modId)) return false;
+    public static final ImmutableList<String> tweakerooFeaturesWatchList = ImmutableList.of(
+            // contains tweakfork
+            "accurate_block_placement",
+            "block_reach_override",
+            "container_scan",
+            "fake_sneaking",
+            "fast_block_placement",
+            "flexible_block_placement",
+            "free_camera",
+            "gamma_override",
+            "no_sneak_slowdown",
+            "scaffold_place"
+    );
+
+    public static final ImmutableList<String> tweakerooYeetsWatchList = ImmutableList.of(
+            // contains masa addition
+            "honey_block_slowdown",
+            "honey_block_jumping",
+            "slime_block_bouncing",
+            "slime_block_slowdown"
+    );
+
+    public static final ImmutableList<String> tweakermoreWatchList = ImmutableList.of(
+            "auto_pick_schematic_block",
+            "disable_darkness_effect",
+            "fake_night_vision",
+            "schematic_block_placement_restriction",
+            "schematic_pro_place"
+    );
+
+    public static final ImmutableList<String> litematicaWatchList = ImmutableList.of(
+            "easy_place_mode",
+            "placement_restriction"
+    );
+
+    public static boolean isFeatureEnable(String className, String fieldName) {
         try {
             Class<?> clazz = Class.forName(className);
             Field field = clazz.getDeclaredField(fieldName);
@@ -28,8 +64,15 @@ public class ClientModUtil {
     }
 
     public static HashMap<String, Boolean> createConfigDataMap() {
+        FabricLoader loader = FabricLoader.getInstance();
         HashMap<String, Boolean> map = new HashMap<>();
 
+        if (loader.isModLoaded(AmaCarpet.ModIds.tweakeroo)) {
+        } if (loader.isModLoaded(AmaCarpet.ModIds.tweakermore)) {
+
+        } if (loader.isModLoaded(AmaCarpet.ModIds.litematica)) {
+
+        }
 
         return map;
     }
