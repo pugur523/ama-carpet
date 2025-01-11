72adf0706ed475e4eb2968f18df13a34db25c724
pugur <ryota52399@gmail.com>
2025-01-11 14:00:54 +0900

    small updates

--- src/main/java/org/amateras_smp/amacarpet/utils/ResourceLocationUtil.java
+++ src/main/java/org/amateras_smp/amacarpet/utils/ResourceLocationUtil.java
@@ -1,6 +1,6 @@
 // Copyright (c) 2025 The AmaCarpet Authors
 // This file is part of the AmaCarpet project and is licensed under the terms of
-// the GNU Lesser General Public License, version 3. See the LICENSE file for details.
+// the GNU Lesser General Public License, version 3.0. See the LICENSE file for details.
 
 package org.amateras_smp.amacarpet.utils;
 
